7eefa2cea4499c58e4f1b500cad42ca882319eb8
Manuel <sufferdarkness44@gmmail.com>
2019-11-25 23:23:35 -0600

    'examen-commit'

---
+++ Examen_Gato.js
@@ -0,0 +1,101 @@
+
+$(document).ready(function()
+{
+
+   $(".container2").hide();
+   $("#barra-progreso").hide();
+   $(".puntos").hide();
+   $('.letras div').css({'opacity':'0%'});
+     var cont=0;
+     var jugadorX=0;
+     var jugadorO=0;
+
+ $(".cuadrado").click(function()
+	{
+        
+  if (cont==0) {
+  $(this).html("O");
+        cont=1;
+        
+        }
+
+else
+{
+$(this).html("X");
+ cont=0;
+ 
+}
+
+if ($("#Cuno").html()=="O" && $("#Cdos").html()=="O" && $("#Ctres").html()=="O"||
+$("#Ccuatro").html()=="O" && $("#Ccinco").html()=="O" && $("#Cseis").html()=="O"||
+$("#Csiete").html()=="O" && $("#Ceight").html()=="O" && $("#Cnueve").html()=="O"||
+$("#Cuno").html()=="O" && $("#Ccinco").html()=="O" && $("#Cnueve").html()=="O"||
+$("#Ctres").html()=="O" && $("#Ccinco").html()=="O" && $("#Csiete").html()=="O"|| 
+$("#Cuno").html()=="O" && $("#Ccuatro").html()=="O" && $("#Csiete").html()=="O"||
+$("#Cdos").html()=="O" && $("#Ccinco").html()=="O" && $("#Cocho").html()=="O"||
+$("#Ctres").html()=="O" && $("#Cseis").html()=="O" && $("#Cnueve").html()=="O") {
+  alert("Ganó O");
+$("#Cuno").empty();
+$("#dos").empty();
+$("#tres").empty();
+$("#Ccuatro").empty();
+$("#Ccinco").empty();
+$("#Cseis").empty();
+$("#Csiete").empty();
+$("#Ceight").empty();
+$("#nueve").empty();
+ jugadorO++;
+ $(".espanO").html(jugadorO);
+}
+
+
+else if (
+  $("#Cuno").html()=="X" && $("#Cdos").html()=="X" && $("#Ctres").html()=="X"||
+$("#Ccuatro").html()=="X" && $("#Ccinco").html()=="X" && $("#Cseis").html()=="X"||
+$("#Csiete").html()=="X" && $("#Ceight").html()=="X" && $("#Cnueve").html()=="X"||
+$("#Cuno").html()=="X" && $("#Ccinco").html()=="X" && $("#Cnueve").html()=="X"||
+$("#Ctres").html()=="X" && $("#Ccinco").html()=="X" && $("#Csiete").html()=="X"|| 
+$("#Cuno").html()=="X" && $("#Ccuatro").html()=="X" && $("#Csiete").html()=="X"||
+$("#Cdos").html()=="X" && $("#Ccinco").html()=="X" && $("#Cocho").html()=="X"||
+$("#Ctres").html()=="X" && $("#Cseis").html()=="X" && $("#Cnueve").html()=="X") {
+  alert("Ganó X");
+$("#Cuno").empty();
+$("#dos").empty();
+$("#tres").empty();
+$("#Ccuatro").empty();
+$("#Ccinco").empty();
+$("#Cseis").empty();
+$("#Csiete").empty();
+$("#Ceight").empty();
+$("#nueve").empty();
+jugadorX++;
+ $(".espanX").html(jugadorX);
+}
+
+
+
+});
+
+});
+ 
+
+
+
+    function animar()
+	{
+		$("#boton").hide();
+		$("#barra-progreso").show();
+      document.getElementById("hola").classList.toggle("final");
+      $('.letras div').css({'opacity':'100%'});
+      $("#barra-progreso").delay(10000).fadeOut('slow');
+       $(".letras").delay(10000).fadeOut('slow');
+       $(".container2").delay(11000).fadeIn('slow');
+       $(".puntos").delay(11000).fadeIn('slow');
+	}
+
+
+  
+   
+  
+
+
